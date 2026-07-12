6342059a9e78190e24b2519bfe6aa56785a5ffc9
hulaanmo <inhog.cedrick@gmail.com>
2026-07-12 20:56:44 +0800

    add basic flow control example from book

--- part-1/02-flow-control/main.py
+++ part-1/02-flow-control/main.py
@@ -1,6 +1,18 @@
 def main():
     print("Hello from flow-control!")
 
+    code_block()
+
+def code_block():
+    username = 'Mary'
+    password = 'swordfish'
+
+    if username == 'Mary':
+        print('Hello, Mary!')
+        if password == 'swordfish':
+            print('Access granted.')
+        else:
+            print('Wrong password')
 
 if __name__ == "__main__":
     main()
