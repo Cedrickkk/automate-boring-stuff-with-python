97ba8250b14c9455ccb3bf7687f2d0951286ffa5
hulaanmo <inhog.cedrick@gmail.com>
2026-07-12 10:38:54 +0800

    add python number methods

--- part-1/01-python-basics/main.py
+++ part-1/01-python-basics/main.py
@@ -9,5 +9,16 @@ def main():
     age = input("> ")
     print("You will be " + str(int(age) + 1) + " in a year.")
 
+    print("\nNumber Methods in Python: ")
+    number_methods()
+
+def number_methods():
+    print(round(3.14))
+    print(round(7.7))
+    print(round(-2.2))
+
+    print(round(3.14, 1))
+    print(round(7.7777, 3))
+
 if __name__ == "__main__":
     main()
