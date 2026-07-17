957b289898e4c0d225187cc049bb852704270b03
hulaanmo <inhog.cedrick@gmail.com>
2026-07-17 14:03:02 +0800

    add truthy and falsy values example in a loop

---
+++ part-1/03-loops/truthy_and_falsy_values.py
@@ -0,0 +1,17 @@
+def main() -> None:
+    name = ''
+    
+    while not name:
+        print('Enter your name: ')
+        name = input("> ")
+
+    print("How many guests will you have?")
+    num_of_guests = int(input("> "))
+
+    if num_of_guests:
+        print("Be sure to have enough room for all your guests.")
+    
+    print("Done")
+
+if __name__ == '__main__':
+    main()
