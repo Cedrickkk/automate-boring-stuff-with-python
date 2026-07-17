7554cfe08319486a171ba0c742b0f6d128d7502d
hulaanmo <inhog.cedrick@gmail.com>
2026-07-17 13:39:25 +0800

    add basic while loop example from textbook

--- part-1/03-loops/main.py
+++ part-1/03-loops/main.py
@@ -2,5 +2,14 @@ def main():
     print("Hello from loops!")
 
 
+    while_loop_test()
+    
+def while_loop_test() -> None:
+    spam = 0
+    
+    while spam < 5:
+        print("Hello, world.")
+        spam += 1
+
 if __name__ == "__main__":
     main()
