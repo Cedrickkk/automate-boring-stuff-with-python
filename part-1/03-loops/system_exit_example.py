5f42c638abd5092b856d11f78a3a18d7287a66e1
hulaanmo <inhog.cedrick@gmail.com>
2026-07-25 13:04:21 +0800

    add system exit example using infinite loop

---
+++ part-1/03-loops/system_exit_example.py
@@ -0,0 +1,15 @@
+import sys
+
+def main() -> None:
+    while True:
+        print("Type 'exit' to exit.")
+        response = input("> ")
+
+        if response == "exit":
+            sys.exit()
+
+        print(f"You typed '{response}'")
+
+
+if __name__ == "__main__":
+    main()
