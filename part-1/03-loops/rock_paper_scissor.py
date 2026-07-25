b87e685d4c1ccdd1a9687ce1517c47755d7c8cfc
hulaanmo <inhog.cedrick@gmail.com>
2026-07-25 14:13:43 +0800

    refactor rock, paper, scissors game

--- part-1/03-loops/rock_paper_scissor.py
+++ part-1/03-loops/rock_paper_scissor.py
@@ -1,6 +1,12 @@
 import sys
 
-from random import randint
+from random import choice
+
+MOVES = {
+    "r": "ROCK",
+    "p": "PAPER",
+    "s": "SCISSORS"
+}
 
 def main() -> None:
     print("ROCK, PAPER, SCISSORS")
@@ -9,64 +15,54 @@ def main() -> None:
     losses: int = 0
     ties: int = 0
 
-    player: str | None = None
-    computer: str | None = None
-
     while True:
         print(f"{wins} Wins {losses} Losses {ties} Ties")
-        while True:
-            print("Enter your move: \n(r) rock \n(p) paper \n(s) scissors \n(q) quit")
-            player = input("> ")
-    
-            if player == 'q':
-                sys.exit()
-    
-            if player == 'r' or player == 'p' or player == 's':
-                break
-    
-            print("Type one of r, p, s, or q.")
-            
-        if player == 'r':
-            print("ROCK versus ...")
-        elif player == 'p':
-            print("PAPER versus ...")
-        elif player == 's':
-            print("SCISSORS versus ...")
-    
-        random_number = randint(1, 3)
-    
-        if random_number == 1:
-            computer = 'r'
-            print("ROCK")
-        elif random_number == 2:
-            computer = 'p'
-            print("PAPER")
-        elif random_number == 3:
-            computer = 's'
-            print("SCISSORS")
+        player = get_player_move()
+        computer = get_computer_move()  
+        print(f"{MOVES[player]} versus {MOVES[computer]}")
+        
+        result =  determine_round_result(player, computer)
 
-        if player == computer:
+        if result == "tie":
             print("It is a tie!")
             ties += 1
-        elif player == 'r' and computer == 's':
+        elif result == "win":
             print("You win!")
             wins += 1
-        elif player == 'p' and computer == 'r':
-            print("You win!")
-            wins += 1
-        elif player == 's' and computer == 'p':
-            print("You win!")
-            wins += 1
-        elif player == 'r' and computer == 'p':
-            print("You lose!")
-            losses -= 1
-        elif player == 'p' and computer == 's':
-            print("You lose!")
-            losses -= 1
-        elif player == 's' and computer == 'r':
-            print("You lose!")
-            losses -= 1
-        
-        
+        else:
+            print("You lose!") 
+            losses += 1
+
+def get_player_move() -> str:
+    while True:
+        print("Enter your move: \n(r) rock \n(p) paper \n(s) scissors \n(q) quit")
+        move = input("> ")
+
+        if move == 'q':
+            sys.exit()
+
+        if move in MOVES:
+            return move
+
+        print("Type one of r, p, s, or q.")
+            
+def get_computer_move() -> str:
+    return choice(['r', 'p', 's'])
+
+def determine_round_result(player: str, computer: str) -> str:
+    if player == computer:
+        return "tie" 
+
+    wins_against = {
+        ("r", "s"),
+        ("p", "r"),
+        ("s", "p")
+    }
+
+    if (player, computer) in wins_against:
+        return "win"
+
+    return "lose"
+
 if __name__ == "__main__":
     main()
