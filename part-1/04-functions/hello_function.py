13eaee8b3e2c9b30f014d7841989281a1df5ece1
hulaanmo <inhog.cedrick@gmail.com>
2026-07-25 15:37:05 +0800

    add hello function and main execution flow

---
+++ part-1/04-functions/hello_function.py
@@ -0,0 +1,13 @@
+def main() -> None:
+    hello()
+    hello()
+    hello()
+
+def hello() -> None:
+    # Print three greetings
+    print("Good morning!")
+    print("Good afternoon!")
+    print("Good evening!")
+
+if __name__ == "__main__":
+    main()
