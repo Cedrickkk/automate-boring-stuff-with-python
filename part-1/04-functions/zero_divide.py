238cd429faeabc8286823e8a578ae13ce27189b6
hulaanmo <inhog.cedrick@gmail.com>
2026-07-26 20:17:14 +0800

    refactor spam function error handling to be handled in main

--- part-1/04-functions/zero_divide.py
+++ part-1/04-functions/zero_divide.py
@@ -1,14 +1,14 @@
 def main() -> None:
-    print(spam(2))
-    print(spam(12))
-    print(spam(0))
-    print(spam(4))
-
-def spam(divide_by: float) -> float | None:
     try:
-        return 42 / divide_by
+        print(spam(2))
+        print(spam(12))
+        print(spam(0))
+        print(spam(4))
     except ZeroDivisionError:
         print("Error: Invalid argument.")
+
+def spam(divide_by: float) -> float:
+    return 42 / divide_by
 
 if __name__ == "__main__":
     main()
