4e3af4de4a7f24e56d87c0aa860b24a025b0df63
hulaanmo <inhog.cedrick@gmail.com>
2026-07-31 08:25:40 +0800

    update logging configuration to write to a file

--- part-1/05-debugging/factorial_log.py
+++ part-1/05-debugging/factorial_log.py
@@ -1,6 +1,6 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename="factorial_log.txt",level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main() -> None:
     logging.debug("Start of program")
