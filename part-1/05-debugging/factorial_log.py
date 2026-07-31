3c4beb139ed1df2661556d1cbe92bee57511a964
hulaanmo <inhog.cedrick@gmail.com>
2026-07-31 08:24:59 +0800

    add factorial_log script with logging functionality

---
+++ part-1/05-debugging/factorial_log.py
@@ -0,0 +1,21 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main() -> None:
+    logging.debug("Start of program")
+    num = int(input("Enter a number: "))
+    print(factorial(num))
+    logging.debug("End of program")
+
+def factorial(n: int) -> int:
+    logging.debug(f"Start of factorial({n})")
+    total: int = 1
+    for i in range(1, n + 1):
+        total *= i
+        logging.debug(f"i is {i}, total is {total}")
+    logging.debug(f"End of factorial({n})")
+    return total
+
+if __name__ == "__main__":
+    main()
