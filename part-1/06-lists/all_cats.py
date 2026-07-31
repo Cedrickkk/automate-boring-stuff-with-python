f22198b1fbc2e607afc5f1ff98ca8a75b5728e72
hulaanmo <inhog.cedrick@gmail.com>
2026-07-31 14:18:31 +0800

    add all_cats.py to demonstrate input and display of cat names

---
+++ part-1/06-lists/all_cats.py
@@ -0,0 +1,10 @@
+def main() -> None:
+    cat1 = input("Enter the name of cat 1: ")
+    cat2 = input("Enter the name of cat 2: ")
+    cat3 = input("Enter the name of cat 3: ")
+    cat4 = input("Enter the name of cat 4: ")
+    print(f"The cat names are {cat1}, {cat2}, {cat3}, {cat4}")
+    
+
+if __name__ == "__main__":
+    main()
