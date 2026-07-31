90f9e23d29ef2fa569f948919572a789557b5ede
hulaanmo <inhog.cedrick@gmail.com>
2026-07-31 09:48:42 +0800

    add type hints for better clarity in indexes.py

--- part-1/06-lists/indexes.py
+++ part-1/06-lists/indexes.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 def main() -> None:
     spam = ["cat", "bat", "rat", "elephant"]
 
@@ -7,6 +9,11 @@ def main() -> None:
     print(spam[3])
 
     print(f"The {spam[1]} ate the {spam[0]}")
+
+    matrix:list[list[Any]] = [["cat", "bat"], [10, 20, 30, 40, 50]]
+    print(matrix[0])
+    print(matrix[0][1])
+    print(matrix[1][4])
 
 if __name__ == "__main__":
     main()
