619cc4adc8d2387c75ad2d37b7af1409c1c9b00d
hulaanmo <inhog.cedrick@gmail.com>
2026-08-02 17:43:13 +0800

    feat: add sequence data types example in sequence_data_types.py

---
+++ part-1/06-lists/sequence_data_types.py
@@ -0,0 +1,16 @@
+def main() -> None:
+    name: str = "Zophie"
+
+    print(name[0])
+    print(name[-2])
+    print(name[0:4])
+    print("Zo" in name)
+    print("z" in name)
+    print("p" not in name)
+
+    for i in name:
+        print(f"* * * {i} * * *")
+
+
+if __name__ == "__main__":
+    main()
