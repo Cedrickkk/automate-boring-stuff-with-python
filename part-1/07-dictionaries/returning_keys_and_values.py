0b1fd4f175edab16f61eae71412043c7108f8f04
hulaanmo <inhog.cedrick@gmail.com>
2026-08-08 12:48:08 +0800

    feat: add returning_keys_and_values.py with dictionary operations example

---
+++ part-1/07-dictionaries/returning_keys_and_values.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+def main() -> None:
+    spam: dict[str, Any] = {"color": "red", "age": 42}
+
+    # iterate through the dictionary values
+    for v in spam.values():
+        print(v)
+
+    # iterate through the dictionary keys
+    for k in spam.keys():
+        print(k)
+
+    # check if given key exists in dictionary keys
+    print("color" in spam.keys())
+    print("age" not in spam.keys())
+
+    # check if given value exists in dictionary values
+    print("red" in spam.keys())
+
+    # get item(tuple) in dictionary
+    for i in spam.items():
+        print(i)
+        print(type(i) is tuple)
+
+    
+if __name__ == "__main__":
+    main()
