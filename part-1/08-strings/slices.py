3f4ae723b1448cec213e2b3757ac22e7bfd200c7
hulaanmo <inhog.cedrick@gmail.com>
2026-08-11 14:21:42 +0800

    feat: add slices.py for string slicing examples

---
+++ part-1/08-strings/slices.py
@@ -0,0 +1,12 @@
+def main() -> None:
+    greet: str = "Hello, World"
+
+    print(greet[0])
+    print(greet[0:5])
+    print(greet[0:5])
+    print(greet[7:-1])
+    print(greet[7:])
+    
+
+if __name__ == "__main__":
+    main()
