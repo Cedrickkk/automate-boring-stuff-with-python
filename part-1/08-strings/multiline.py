da941264c8aef3f04f422b961eb9e23c7d9efa0c
hulaanmo <inhog.cedrick@gmail.com>
2026-08-11 14:19:07 +0800

    feat: add multiline.py with main and hello functions for string examples

---
+++ part-1/08-strings/multiline.py
@@ -0,0 +1,24 @@
+def main() -> None:
+    # Multiline string
+    print("""
+        Deart Alice, 
+        
+        Can you feed Eve's cat this weekend?
+        
+        Sincerely,
+        Bob
+        """)
+
+    # Multiline comment
+    """This is a multiline comment"""
+
+def hello() -> None:
+    """
+    This function just prints hello
+    It doesn't return anything.
+    """
+    print("Hello!")
+    
+
+if __name__ == "__main__":
+    main()
