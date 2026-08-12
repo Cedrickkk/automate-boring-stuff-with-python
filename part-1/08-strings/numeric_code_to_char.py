03fdab4cb6317fbc1859e512314b7316180c3da9
hulaanmo <inhog.cedrick@gmail.com>
2026-08-12 09:01:19 +0800

    feat: add numeric_code_to_char.py for demonstrating character encoding and decoding methods

---
+++ part-1/08-strings/numeric_code_to_char.py
@@ -0,0 +1,16 @@
+def main() -> None:
+    # ordinal value
+    print(ord("A"))
+    print(ord("4"))
+    print(ord("!"))
+
+    # character value
+    print(chr(65))
+
+    # operations
+    print(ord("A") < ord("B"))
+    print(chr(ord('A')))
+    print(chr(ord("A") + 1))
+
+if __name__ == "__main__":
+    main()
