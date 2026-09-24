f9931e73c0acc57719e951ba87d70048a9d33110
hulaanmo <inhog.cedrick@gmail.com>
2026-09-24 09:28:12 +0800

    feat: add bullet point adder implementation

--- projects/02-wiki-markup-bullets/main.py
+++ projects/02-wiki-markup-bullets/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from 02-wiki-markup-bullets!")
+from pyperclip import paste, copy
 
+def main():
+    text: str = paste()
+    lines: list[str] = text.split("\n")
+
+    for i in range(len(lines)):
+        lines[i]  = ' * ' + lines[i]
+
+    text = '\n'.join(lines)
+    
+    copy(text)    
+    print(text)
 
 if __name__ == "__main__":
     main()
